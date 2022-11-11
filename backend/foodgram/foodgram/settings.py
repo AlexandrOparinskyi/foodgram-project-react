3bead5f0bd00551318e9aa662e1d0dbc62930b90
alex <opar1297@mail.ru>
2022-11-11 21:53:19 +0300

    Test server

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^z=9%qo6#*!cb^glf(s^j2(v*ic$x^k!%xnnn#!v(nfyo+*fv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '51.250.99.146'
