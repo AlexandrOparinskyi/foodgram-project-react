cb2a5eec5e5b75134f83d574f13d66c1b55424ca
Александр О <opar1297@mail.ru>
2022-11-12 13:37:37 +0300

    Check work

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-^z=9%qo6#*!cb^glf(s^j2(v*ic$x^k!%xnnn#!v(nfyo+*fv7
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '158.160.11.52'
+    '158.160.11.52',
     'localhost',
     'web',
     'backend',
